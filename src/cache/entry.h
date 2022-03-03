8e86c3483688a4123b0536be6099d7b163abd4e9
EngineersBox <toastcraft.info@gmail.com>
2022-03-03 15:37:33 +1100

    Refactored to use TX_NEW for instance creation

--- src/cache/entry.h
+++ src/cache/entry.h
@@ -7,18 +7,18 @@
 
 #define MAX_CACHE_ENTRIES 10
 
-POBJ_LAYOUT_BEGIN(entries_store);
-POBJ_LAYOUT_ROOT(entries_store, struct Entries);
-POBJ_LAYOUT_TOID(entries_store, CacheEntry);
-POBJ_LAYOUT_END(entries_store);
-
-typedef struct {
+typedef struct CacheEntry {
     int32_t value;
     int64_t timestamp;
 } CacheEntry;
 
-struct Entries {
+typedef struct Entries {
     TOID(CacheEntry) entries[MAX_CACHE_ENTRIES];
-};
+} Entries;
+
+POBJ_LAYOUT_BEGIN(entries_store);
+POBJ_LAYOUT_ROOT(entries_store, struct Entries);
+POBJ_LAYOUT_TOID(entries_store, struct CacheEntry);
+POBJ_LAYOUT_END(entries_store);
 
 #endif //PMEM_MMAP_IO_ENTRY_H
